950da5648260a4514674dae43c7bb7c34be9656e
Candra Darmawan <candra.darmawan@nolimit.id>
2022-09-30 08:45:06 +0700

    add coin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,12 @@ import SMB_Supermushroom from '../assets/image/SMB_Supermushroom.png';
 import Fire_Flower_SMB from '../assets/image/Fire_Flower_SMB.gif';
 import SMB_1_up_Mushroom_Sprite from '../assets/image/SMB_1-up_Mushroom_Sprite.png';
 import Goomba_SMB from '../assets/image/Goomba_SMB.png';
+import Coint from '../assets/image/SMBCoin.gif';
 
-export default function Footer({ onSuper, onLives, onDamage }) {
+export default function Footer({ onCoin, onSuper, onLives, onDamage }) {
   return (
     <div className="footer">
+      <img src={Coint} alt="Coin" className="button" onClick={onCoin}/>
       <img src={SMB_Supermushroom} alt="super_mushroom" className="button" onClick={onSuper}/>
       <img src={SMB_1_up_Mushroom_Sprite} alt="1_up_mushroom_sprite" className="button" onClick={onLives}/>
       <img src={Goomba_SMB} alt="gooba" className="button" onClick={onDamage}/>
